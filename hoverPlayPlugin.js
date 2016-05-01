79fa6287a5dce2021010617c7431e47996537b0b
jayholman <jay@wistia.com>
2016-05-01 13:50:06 -0400

    Hoverplay prototype, no click popover functionality yet though

--- hoverPlayPlugin.js
+++ hoverPlayPlugin.js
@@ -4,24 +4,22 @@ Wistia.plugin("hover-play", function(video, options) {
     // src: "/experiments/magicScrollPlugin.js" - required
 
     //Early Defined Variables
-    var aspectRatio = video.aspect();
     var videoHashedId = video.hashedId();
     //Grab the video container
     var videoContainer = document.getElementsByClassName("wistia_async_" + videoHashedId)[0];
 
     document.onmousemove = function() {
-      var videoPlaying = false;
 
-      var videoLocationInViewport = videoContainer.getBoundingClientRect();
+        var videoLocationInViewport = videoContainer.getBoundingClientRect();
 
-      var mouseX = event.clientX;
-      var mouseY = event.clientY;
+        var mouseX = event.clientX;
+        var mouseY = event.clientY;
 
-      if ((mouseX < videoLocationInViewport.right && mouseX > videoLocationInViewport.left) && (mouseY < videoLocationInViewport.bottom && mouseY > videoLocationInViewport.top) && !videoPlaying) {
-        video.play();
-      } else {
-        video.pause();
-      }
+        if ((mouseX < videoLocationInViewport.right && mouseX > videoLocationInViewport.left) && (mouseY < videoLocationInViewport.bottom && mouseY > videoLocationInViewport.top)) {
+            video.play();
+        } else {
+            video.pause();
+        }
 
     };
 });
