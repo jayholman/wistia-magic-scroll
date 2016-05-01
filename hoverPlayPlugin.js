91a27aeaf6b27b11b878ed7822c073e1d975ffaa
jayholman <jay@wistia.com>
2016-05-01 12:38:15 -0400

    onmouseover and onmouseout cause console promise error with pause

--- hoverPlayPlugin.js
+++ hoverPlayPlugin.js
@@ -1,3 +1,25 @@
 Wistia.plugin("hover-play", function(video, options) {
+    //Current options Available to Change:
+    // video hashed_id -required
+    // src: "/experiments/magicScrollPlugin.js" - required
 
-};
+    //Early Defined Variables
+    var aspectRatio = video.aspect();
+    var videoHashedId = video.hashedId();
+    //Grab the video container
+    var videoContainer = document.getElementsByClassName("wistia_async_" + videoHashedId)[0];
+    //Video Dimensions
+    var videoHeight = video.height();
+    var videoWidth = video.width();
+    //Is the video playing
+    var videoPlaying = false;
+    videoContainer.onmouseover = function() {
+        video.play();
+        videoPlaying = true;
+    };
+    videoContainer.onmouseout = function() {
+        video.pause();
+        videoPlaying = false;
+    };
+
+});
